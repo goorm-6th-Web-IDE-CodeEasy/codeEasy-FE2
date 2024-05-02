4d88c8e097400d5102883eae7e0ab69130d8dc0c
Lee Soyeon <soyeonlee2@gmail.com>
2024-05-02 20:42:05 +0900

    Fix: refactoring app.tsx and themePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import React from 'react';
+import { RecoilRoot } from "recoil";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Layout/Header/Header';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/register';
+import ThemeProvider from "./pages/Theme/ThemeProvider";
+import ThemePage from "./pages/Theme/ThemePage";
 import './App.css';
 
 function App() {
-    return <>코드이지</>;
+  return (
+    <RecoilRoot>
+      <BrowserRouter>
+        <ThemeProvider>
+          <Header />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/theme" element={<ThemePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </RecoilRoot>
+  );
 }
 
 export default App;
--- src/pages/Theme/ThemePage.tsx
+++ src/pages/Theme/ThemePage.tsx
@@ -1,31 +1,31 @@
 import React from "react";
 import ThemeSwitcher from "./ThemeSwitcher";
 import { useTheme } from "@emotion/react";
-import ThemeProvider from "./ThemeProvider";
 
 interface CustomTheme {
   primaryColor: string;
   secondaryColor: string;
 }
-export const ThemePage: React.FC = () => {
+
+const ThemePage: React.FC = () => {
   const theme = useTheme() as CustomTheme;
   return (
-    <ThemeProvider>
-       <div
+    <div
       style={{
         backgroundColor: theme.primaryColor,
         color: theme.secondaryColor,
       }}
     >
-        <h1>테마 설정</h1>
-        <p>
-          모두가 코딩을 즐길 수 있는 공간, CodeEasy! 저희 플랫폼은 저시력자와
-          일반인 모두가 사용하기 편리한 맞춤형 인터페이스를 제공합니다. 사용자가
-          편의대로 다크모드 라이트 모드 및 저시력자를 위한 고대비 모드를
-          제공합니다.
-        </p>
-        <ThemeSwitcher />
-      </div>
-    </ThemeProvider>
+      <h1>테마 설정</h1>
+      <p>
+        모두가 코딩을 즐길 수 있는 공간, CodeEasy! 저희 플랫폼은 저시력자와
+        일반인 모두가 사용하기 편리한 맞춤형 인터페이스를 제공합니다. 사용자가
+        편의대로 다크모드 라이트 모드 및 저시력자를 위한 고대비 모드를
+        제공합니다.
+      </p>
+      <ThemeSwitcher />
+    </div>
   );
 };
+
+export default ThemePage;
