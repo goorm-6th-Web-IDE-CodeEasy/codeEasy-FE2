d29b11bfcdff77f4fbfe94f7ec0d4a8940ac3de1
gimhanyul <yuihan421@gmail.com>
2024-05-02 16:19:28 +0900

    feat:Register,Login구조 생성, GoolgleLoingButton생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import React from 'react';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/register';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
 import './App.css';
 
 function App() {
-    return <>코드이지</>;
+    return (
+        <BrowserRouter>
+         <h1>코드이지</h1>
+          <Routes>
+            <Route path="/" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </BrowserRouter>
+      );
 }
 
 export default App;
