62679e961683c35b6be9b11adb64b701dca5582b
Lee Soyeon <soyeonlee2@gmail.com>
2024-05-02 17:12:36 +0900

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import './App.css';
+// import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Header from './Layout/Header/Header';
 
 function App() {
-    return <>코드이지</>;
+    return (
+        <>
+            <Header />
+            {/* <Routes>
+        <Route path="/home" element={<Main />} />
+        <Route path="/Algorithm" element={<Algorithm />} />
+        <Route path="/Theme" element={<Theme />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register/>} />
+      </Routes> */}
+        </>
+    );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App"; // 확장자 tsx는 생략 가능
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
