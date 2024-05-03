0a8d34ff0c3e07a976911896a97e26f13344bddd
Lee Soyeon <soyeonlee2@gmail.com>
2024-05-03 22:26:18 +0900

    feat: Add main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from "react";
-import { RecoilRoot } from "recoil";
-import Header from "./Layout/Header/Header";
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import Header from './Layout/Header/Header';
+import Main from './pages/Main/Main';
 
 function App() {
-  return (
-    <RecoilRoot>
-      <Header />
-    </RecoilRoot>
-  );
+    return (
+        <RecoilRoot>
+            <HashRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    {/* route 나중에 더 추가하기 알고리즘, 테마, 로그인, 회원가입*/}
+                </Routes>
+            </HashRouter>
+        </RecoilRoot>
+    );
 }
 
 export default App;
---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Main.module.scss';
+
+const Main = () => {
+    return (
+        // 메인 첫 화면 랜딩페이지 작업하기
+        <div> 
+
+
+            
+        <footer>
+            <p>© 2024 CodeEasy. All rights reserved.</p>
+            <ul>
+                <li><a href="#">개인정보 처리방침</a></li>
+                <li><a href="#">서비스 약관</a></li>
+            </ul>
+        </footer>
+    </div>
+    );
+};
+
+export default Main;
