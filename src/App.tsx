94aa46692d5d30d2566adb5420019c3d9ce9814c
jangyootae <sjang1425@naver.com>
2024-05-07 10:42:35 +0900

    feat. css 수정, app.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { RecoilRoot } from "recoil";
+import { ThemePage } from "./pages/Theme/ThemePage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Layout/Header/Header";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/register";
 function App() {
   return (
+    <RecoilRoot>
+      <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/theme" element={<ThemePage />} />
+          </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 }
 
