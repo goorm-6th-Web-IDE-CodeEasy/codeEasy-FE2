c9394703e29fac4650879a331c075eebcf1dffe7
Lee Soyeon <soyeonlee2@gmail.com>
2024-05-07 11:25:14 +0900

    Add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Login from './pages/Login/Login';
+import Login from './pages/Login/login';
 import Register from './pages/Register/register';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
