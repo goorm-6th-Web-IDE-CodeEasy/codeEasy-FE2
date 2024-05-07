7e1c99203159293b2f12662d224d27130e8b56bc
Lee Soyeon <soyeonlee2@gmail.com>
2024-05-07 19:06:39 +0900

    Fix: svg 컴포넌트로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -5,36 +5,32 @@ import { scaleState } from './recoil/state/scaleState';
 import Header from './Layout/Header/Header';
 import Main from './pages/Main/Main';
 import Algorithm from './pages/Algorithm/Algorithm';
-import Login from './pages/Login/login';
-import Register from './pages/Register/register';
 import { ThemePage } from './pages/Theme/ThemePage';
 
 const App: React.FC = () => {
-  return (
-    <RecoilRoot>
-      <AppContent />
-    </RecoilRoot>
-  );
+    return (
+        <RecoilRoot>
+            <AppContent />
+        </RecoilRoot>
+    );
 };
 
 const AppContent: React.FC = () => {
-  const scale = useRecoilValue<number>(scaleState);
+    const scale = useRecoilValue<number>(scaleState);
 
-  return (
-    <HashRouter>
-      <Header />
-      <div style={{ transform: `scale(${scale})`, transformOrigin: 'top left' }}>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/algorithm" element={<Algorithm />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/theme" element={<ThemePage />} />
-          {/* 추가 라우트 경로들 */}
-        </Routes>
-      </div>
-    </HashRouter>
-  );
+    return (
+        <HashRouter>
+            <Header />
+            <div style={{ transform: `scale(${scale})`, transformOrigin: 'top left' }}>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/algorithm" element={<Algorithm />} />
+                    <Route path="/theme" element={<ThemePage />} />
+                    {/* 추가 라우트 경로들 */}
+                </Routes>
+            </div>
+        </HashRouter>
+    );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App"; // 확장자 tsx는 생략 가능
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // 확장자 tsx는 생략 가능
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root")!);
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
