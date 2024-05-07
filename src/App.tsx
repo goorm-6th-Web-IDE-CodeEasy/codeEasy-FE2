feebb05ddaba36e288d3ec7c230d382ac98e8874
Lee Soyeon <soyeonlee2@gmail.com>
2024-05-07 11:31:22 +0900

    Add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import Login from './pages/Login/login';
-import Register from './pages/Register/register';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-
-import './App.css';
-// import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { RecoilRoot, useRecoilValue } from 'recoil'; // RecoilRoot
+import { scaleState } from './recoil/state/scaleState';
 import Header from './Layout/Header/Header';
+import Main from './pages/Main/Main';
+import Algorithm from './pages/Algorithm/Algorithm';
 
 function App() {
     return (
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </BrowserRouter>
-      );
+        <RecoilRoot>
+            <AppContent />
+        </RecoilRoot>
+    );
+}
+
+function AppContent() {
+    const scale = useRecoilValue(scaleState);
+
+    return (
+        <HashRouter>
+            <Header />
+            <div style={{ transform: `scale(${scale})`, transformOrigin: 'top left' }}>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/algorithm" element={<Algorithm />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+
+                    {/* 추가 라우트 경로들 */}
+                </Routes>
+            </div>
+        </HashRouter>
+    );
 }
 
 export default App;
