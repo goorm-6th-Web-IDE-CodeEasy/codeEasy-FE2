0b88d3e7ff5e7d1ef76f549cbf98a5520edf9474
Lee Soyeon <soyeonlee2@gmail.com>
2024-05-09 17:01:17 +0900

    Feat: 문제 리스트 화면 구현

--- src/pages/Algorithm/Algorithm.tsx
+++ src/pages/Algorithm/Algorithm.tsx
@@ -7,6 +7,7 @@ import AlgorithmMainSvg from '../../components/Svg/AlgorithmMainSvg';
 import { ThemeState } from '../Theme/ThemeState';
 import Header from '../../Layout/Header/Header';
 import throttle from 'lodash/throttle';
+import axios from 'axios';
 
 const Algorithm: React.FC = () => {
     const theme = useRecoilValue(ThemeState);
@@ -14,19 +15,30 @@ const Algorithm: React.FC = () => {
     const [isVolumeOn] = useRecoilState<boolean>(soundState);
     const [problems, setProblems] = useState([]);
     const [user, setUser] = useState({ nickname: '게스트' }); // 초기값을 기본 사용자 이름 설정
+    const [randomProblem, setRandomProblem] = useState(null);
 
     useEffect(() => {
-        // 문제 리스트를 불러오는 API
-        fetch('/problemList')
-            .then((response) => response.json())
-            .then((data) => setProblems(data.problems))
-            .catch((error) => console.error('Error fetching problem data: ', error));
+        const fetchData = async () => {
+            try {
+                // 문제 리스트를 불러오는 API
+                const responseProblems = await axios.get('/problemList');
+                setProblems(responseProblems.data.problems);
 
-        // 사용자 프로필을 불러오는 API
-        fetch('/api/user/profile')
-            .then((response) => response.json())
-            .then((data) => setUser(data.user))
-            .catch((error) => console.error('Error fetching user profile: ', error));
+                // 문제리스트 페치되었을때, 랜덤한 문제 불러오기
+                if (responseProblems.data.problems.length > 0) {
+                    const randomIndex = Math.floor(Math.random() * responseProblems.data.problems.length);
+                    setRandomProblem(responseProblems.data.problems[randomIndex]);
+                }
+
+                // 사용자 프로필을 불러오는 API
+                const responseUser = await axios.get('/api/user/profile');
+                setUser(responseUser.data.user);
+            } catch (error) {
+                console.error('Error fetching data: ', error);
+            }
+        };
+
+        fetchData();
     }, []);
 
     //음성 tts 함수
@@ -43,7 +55,13 @@ const Algorithm: React.FC = () => {
                 <Header />
                 <div className={styles.mainSection}>
                     <div className={styles.textContainer}>
-                        <h1 className={styles.h1Title}>{user.nickname}님 오늘도 힘차게 시작해볼까요?</h1>
+                        <h1 className={styles.h1Title}>
+                            반갑습니다, {user.nickname}님<br></br>
+                            오늘도 힘차게 시작해볼까요?
+                        </h1>
+                        <div>
+                            <button className={styles.btnAlgorithm}>문제 풀어보기</button>
+                        </div>
                     </div>
                     <div className={styles.iconContainer}>
                         <AlgorithmMainSvg />
@@ -53,7 +71,7 @@ const Algorithm: React.FC = () => {
                     <div className={styles.problemSection}>
                         {problems.map((problem, index) => (
                             <div key={index} className={styles.problemCard}>
-                                <h2>{problem.title}</h2>
+                                <h3>{problem.title}</h3>
                                 <p>{problem.content}</p>
                                 <p>난이도: {problem.tier}</p>
                                 <p>유형: {problem.type}</p>
@@ -64,14 +82,24 @@ const Algorithm: React.FC = () => {
                     </div>
                     <div className={styles.clientSection}>
                         {user && (
-                            <div className={styles.userProfile}>
-                                <img src={user.avatar} alt="User Avatar" className={styles.userAvatar} />
-                                <div>닉네임: {user.nickname}</div>
-                                <div>레벨: {user.level}</div>
-                                <div>게시물 수: {user.posts}</div>
-                                <div>젬 수: {user.gems}</div>
-                                <div>완료된 챌린지: {user.completedChallenges}</div>
-                            </div>
+                            <>
+                                <div className={styles.clientAvatar}>
+                                    <img src={user.avatar} alt="User Avatar" className={styles.userAvatar} />
+                                </div>
+                                <div className={styles.userProfile}>
+                                    <div>닉네임: {user.nickname}</div>
+                                    <div>레벨: {user.level}</div>
+                                    <div>게시물 수: {user.posts}</div>
+                                    <div>젬 수: {user.gems}</div>
+                                    <div>완료된 챌린지: {user.completedChallenges}</div>
+                                </div>
+                                {randomProblem && (
+                                    <div className={styles.todayAlgorithm}>
+                                        <p className={styles.title}>{user.nickname}님을 위한 오늘의 추천 알고리즘</p>
+                                        <div>{randomProblem.title}</div>
+                                    </div>
+                                )}
+                            </>
                         )}
                     </div>
                 </div>
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -26,7 +26,7 @@ const Main: React.FC = () => {
             const speech = new SpeechSynthesisUtterance(text);
             window.speechSynthesis.speak(speech);
         }
-    }, 4000);
+    }, 3000);
 
     return (
         <div className={`${theme}`}>
@@ -92,7 +92,7 @@ const Main: React.FC = () => {
                     </h1>
                     <p className={styles.text}>
                         다양한 난이도의 문제들로 실력을 시험하고 코딩 능력을 키워보세요. 데이터 구조부터 복잡한 알고리즘
-                        문제까지, 초보자부터 경력자까지 모두를 위한 문제들이 준비되어 있습니다.
+                        문제까지, 모두를 위한 문제들이 준비되어 있습니다.
                     </p>
                     <Link>
                         <button className={styles.btnAlgorithm}>문제 풀어보기</button>
