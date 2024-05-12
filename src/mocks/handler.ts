3a902a371977562dac5bdf1d56acd8ffb9993c1c
gimhanyul <yuihan421@gmail.com>
2024-05-12 20:53:34 +0900

    chore: msw test + loggedInState 관리

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,52 +1,80 @@
 // handler.ts
-import { rest } from 'msw';
-import { mockData } from './data';
+import { rest } from 'msw'
+import { AlgorithmData, users } from './data'
 
 export const handlers = [
     rest.get('/problemList', (req, res, ctx) => {
-        return res(ctx.status(200), ctx.json({ problems: mockData.problems }));
+        return res(ctx.status(200), ctx.json({ problems: AlgorithmData.problems }))
     }),
     rest.get('/problemList/done', (req, res, ctx) => {
-        const doneProblems = mockData.problems.filter((problem) => problem.done);
-        return res(ctx.status(200), ctx.json({ problems: doneProblems }));
+        const doneProblems = AlgorithmData.problems.filter((problem) => problem.done)
+        return res(ctx.status(200), ctx.json({ problems: doneProblems }))
     }), // problemList/tier
     rest.get('/problemList/tier', (req, res, ctx) => {
-        const tier = req.url.searchParams.get('tier');
+        const tier = req.url.searchParams.get('tier')
         const filteredProblems = tier
-            ? mockData.problems.filter((problem) => problem.tier === tier)
-            : mockData.problems;
-        return res(ctx.status(200), ctx.json({ problems: filteredProblems }));
+            ? AlgorithmData.problems.filter((problem) => problem.tier === tier)
+            : AlgorithmData.problems
+        return res(ctx.status(200), ctx.json({ problems: filteredProblems }))
     }),
     rest.get('/problemList/algorithm', (req, res, ctx) => {
-        const type = req.url.searchParams.get('type') || 'Algorithm';
-        const algorithmProblems = mockData.problems.filter((problem) => problem.type === type);
-        return res(ctx.status(200), ctx.json({ problems: algorithmProblems }));
+        const type = req.url.searchParams.get('type') || 'Algorithm'
+        const algorithmProblems = AlgorithmData.problems.filter((problem) => problem.type === type)
+        return res(ctx.status(200), ctx.json({ problems: algorithmProblems }))
     }),
     rest.get('/problemList/rate', (req, res, ctx) => {
-        const rate = req.url.searchParams.get('rate');
+        const rate = req.url.searchParams.get('rate')
         const filteredProblems = rate
-            ? mockData.problems.filter((problem) => problem.successRate === rate)
-            : mockData.problems;
-        return res(ctx.status(200), ctx.json({ problems: filteredProblems }));
+            ? AlgorithmData.problems.filter((problem) => problem.successRate === rate)
+            : AlgorithmData.problems
+        return res(ctx.status(200), ctx.json({ problems: filteredProblems }))
     }),
 
-    //user api 구현
+    //login
+    rest.post('/api/login', (req, res, ctx) => {
+        const { email, password } = req.body as { email: string; password: string }
+        const user = users.find((u) => u.email === email && u.password === password)
+
+        if (!user) {
+            return res(ctx.status(401), ctx.json({ errorMessage: '이메일 또는 비밀번호가 잘못되었습니다.' }))
+        }
+
+        return res(
+            ctx.json({
+                message: '로그인 성공',
+                id: user.id,
+                email: user.email,
+                nickname: user.nickname,
+                tier: user.tier,
+                token: 'df',
+                posts: user.posts,
+                completedChallenges: user.completedChallenges,
+                avatar: user.avatar,
+            }),
+        )
+    }),
+    //user/profile
     rest.get('/api/user/profile', (req, res, ctx) => {
+        const { email } = req.body as { email: string }
+        const user = users.find((u) => u.email === email)
+
+        if (!user) {
+            return res(ctx.status(404), ctx.json({ errorMessage: '사용자를 찾을 수 없습니다.' }))
+        }
         return res(
             ctx.status(200),
             ctx.json({
                 user: {
-                    level: 100,
-                    nickname: '말티푸',
-                    posts: 1024,
-                    gems: 100002,
-                    completedChallenges: 80,
-                    avatar: 'https://source.unsplash.com/featured/?{puppy}',
+                    tier: user.tier,
+                    nickname: user.nickname,
+                    posts: user.posts,
+                    completedChallenges: user.completedChallenges,
+                    avatar: user.avatar,
                 },
-            })
-        );
+            }),
+        )
     }),
-];
+]
 
 // 위의 설정은 각 필터 옵션에 맞게 요청을 처리하고 해당하는 데이터를 반환.
 // 'tier', 'type', 'rate' 각각의 필터링은 쿼리 파라미터를 받아 해당 조건에 맞는 데이터를 필터링.
