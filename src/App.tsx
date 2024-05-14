e57df17f30aaaee184f5d65e1f5eb7cd069161fc
Lee Soyeon <soyeonlee2@gmail.com>
2024-05-14 18:50:56 +0900

    Feat: Refactor routing in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,15 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import { ThemePage } from './pages/Theme/ThemePage';
 
+const ROUTES_INFO = [
+    { path: '/', element: <Main /> },
+    { path: '/algorithm', element: <Algorithm /> },
+    { path: '/login', element: <Login /> },
+    { path: '/register', element: <Register /> },
+    { path: '/theme', element: <ThemePage /> },
+    // 추가 라우트 경로들
+];
+
 const App: React.FC = () => {
     return (
         <RecoilRoot>
@@ -24,12 +33,9 @@ const AppContent: React.FC = () => {
             {/* 헤더와 푸터 각 페이지마다 따로 넣기 */}
             <div style={{ transform: `scale(${scale})`, transformOrigin: 'top left' }}>
                 <Routes>
-                    <Route path="/" element={<Main />} />
-                    <Route path="/algorithm" element={<Algorithm />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/theme" element={<ThemePage />} />
-                    {/* 추가 라우트 경로들 */}
+                    {ROUTES_INFO.map((route) => (
+                        <Route key={route.path} path={route.path} element={route.element} />
+                    ))}
                 </Routes>
             </div>
         </HashRouter>
